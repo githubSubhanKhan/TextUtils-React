6e649ba869a51ccf827cd8f54f873488945d7b96
Muhammad Subhan Khan <subbikhan124@gmail.com>
2024-12-01 21:25:13 +0500

    removed home from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,15 +11,15 @@ export default function Navbar(props) {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+          {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0"> */}
+            {/* <li className="nav-item"> */}
               {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
+              {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
+            {/* </li> */}
             {/* <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li> */}
-          </ul>
+          {/* </ul> */}
           {/* <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button className="btn btn-outline-success" type="submit">Search</button>
